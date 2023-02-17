36f1a1a05877a10eff45e24af7d86051794972b6
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-02-17 01:31:19 +0100

    Create izzatexpress.js

---
+++ izzatexpress.js
@@ -0,0 +1,1 @@
+ var dominiosAutorizados = ['izzatexpress.com.br', 'izzatexpress.com']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }  
