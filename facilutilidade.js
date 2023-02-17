220f03648b18e2fbee381281940af306e68f83b5
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-02-17 01:43:19 +0100

    Update facilutilidade.js

--- facilutilidade.js
+++ facilutilidade.js
@@ -1,1 +1,1 @@
-oi
+ var dominiosAutorizados = ['facilutilidade.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }  
