3a594d14e19ec9a1d5731916a9e1f1ba6459f7cc
BrunnoKz <74139100+BrunnoKz@users.noreply.github.com>
2023-03-10 17:32:56 +0100

    Update bloqueio.js

--- bloqueio.js
+++ bloqueio.js
@@ -1,1 +1,1 @@
- var dominiosAutorizados = ['www.niarshop.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }  
+ var dominiosAutorizados = ['niarshop.com.br']; if (dominiosAutorizados.indexOf(window.location.hostname) === -1) {   window.location.href = 'https://dashotimiza.com/dmca'; }  
